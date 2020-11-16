6155163920cd28c0adbd021d1aa98e2bd561f0bb
maruchan1381 <haru0328mh@gmail.com>
2020-11-16 10:57:03 +0900

    ログイン、ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
-  
-  validates :nickanme, presence: true
+
+  validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 },
   format: { with:  /\A[a-z0-9]+\z/}
