792d1021c379af1abd1876abc988e72f34d0ff9a
maruchan1381 <haru0328mh@gmail.com>
2020-11-18 18:35:09 +0900

    バリデーションの修正（with_options)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 },
-  format: { with:  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :last_name, presence: true,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true,
-  format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true,
-  format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
-
+  
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 },
+    format: { with:  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana,format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 end
