0cfbac01ee779955bcc778a3421afb2bc1f59257
maruchan1381 <haru0328mh@gmail.com>
2020-11-26 12:00:48 +0900

    コントローラー、ビュー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    # @items = Item.all(商品機能一覧機能実装)
+    @items = Item.all
   end
 
   def new
